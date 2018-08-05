bde024ed72a8be085bcb5dc9c51ee1c5e6c65445
LAPTOP-9NHTP27O\saurabh <samakkar25@gmail.com>
2018-08-05 09:28:56 +0530

    calling webservices from another webservice. E2E demo available here. EurekaClient1.java calling webservice from EurekaClient2.java

--- spring-cloud-eureka-client/src/main/java/com/baeldung/spring/cloud/spring_cloud_eureka_client/EurekaClient1.java
+++ spring-cloud-eureka-client/src/main/java/com/baeldung/spring/cloud/spring_cloud_eureka_client/EurekaClient1.java
@@ -1,23 +1,76 @@
 package com.baeldung.spring.cloud.spring_cloud_eureka_client;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import com.netflix.appinfo.InstanceInfo;
+import com.netflix.discovery.EurekaClient;
+import com.netflix.discovery.shared.Application;
 
 
 
+
+//We can use both @EnableEurekaClient OR @EnableDiscoveryClient
 @SpringBootApplication
 @EnableEurekaClient
 @RestController
 public class EurekaClient1 {
-    public static void main(String[] args) {
+	
+    @Autowired
+    private DiscoveryClient eurekaClient;
+
+//    @Autowired
+//    private EurekaClient eurekaClient;
+    
+    @Autowired
+    private RestTemplate restTemplate;
+    
+    public static void main(String[] args) throws InterruptedException {
         SpringApplication.run(EurekaClient1.class, args);
     }
  
-    @RequestMapping("/greeting")
-    public String greeting() {
-        return "Hello from EurekaClient!";
+
+	@RequestMapping("/xyz")
+	public String greeting1() {
+		
+		// To run this below url with local host, we need to initialize rest template with new keyword
+		//not with autowired
+		//reason don't know
+		String uri1 = "http://localhost:8098/allstudentxml";
+		String response1 = restTemplate.exchange(uri1, HttpMethod.GET, null, new ParameterizedTypeReference<String>() {
+		}).getBody();
+		System.out.println(response1);
+		
+		
+		// To run this below url with registered service name, we need to initialize rest template with 
+		//autowired and not with new keyword
+		//This is because rest template will gets initiliazed in the spring context and from there other 
+		//objects will pick it. With new keu=yword it will not be identified
+		String uri2 = "http://student-service/studentjson";
+		String response2 = restTemplate.exchange(uri2, HttpMethod.GET, null, new ParameterizedTypeReference<String>() {}).getBody();
+		System.out.println(response2);
+		return "Hello from EurekaClient!";
+	}
+	
+	
+    
+    @Bean
+    @LoadBalanced
+    public RestTemplate getTemplate() {
+    	return new RestTemplate();
     }
 }
--- spring-cloud-eureka-server/src/main/java/com/baeldung/spring/cloud/spring_cloud_eureka_server/EurekaServerApp.java
+++ spring-cloud-eureka-server/src/main/java/com/baeldung/spring/cloud/spring_cloud_eureka_server/EurekaServerApp.java
@@ -1,8 +1,13 @@
 package com.baeldung.spring.cloud.spring_cloud_eureka_server;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+import com.netflix.appinfo.InstanceInfo;
+import com.netflix.discovery.EurekaClient;
+import com.netflix.discovery.shared.Application;
 
 /**
  * Hello world!
@@ -11,7 +16,29 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @SpringBootApplication
 @EnableEurekaServer
 public class EurekaServerApp {
+	
+    @Autowired
+    private static EurekaClient eurekaClient;
+    
+    public static void doRequest() {
+    	
+    	if(eurekaClient==null) {
+    		System.out.println("null");
+    		return;
+    	}
+    	
+        Application application = eurekaClient.getApplication("student-service");
+        InstanceInfo instanceInfo = application.getInstances().get(0);
+        String hostname = instanceInfo.getHostName();
+        int port = instanceInfo.getPort();
+        System.out.println(hostname);
+        System.out.println(port);
+        
+    }
+    
+    
 	public static void main(String[] args) {
 		SpringApplication.run(EurekaServerApp.class, args);
+		doRequest();
 	}
 }
